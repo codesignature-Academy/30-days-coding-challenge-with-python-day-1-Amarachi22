7496958dbb0ce74695b819d26eb5b72de52e0cfb
amarachi <giftmonday421@gmail.com>
2025-11-13 18:21:37 +0100

    assignment completed pls rate

---
+++ day1/assignment.py
@@ -0,0 +1,26 @@
+full_name = input("enter your full name? ")
+age = input("age? ")
+residence = input("where do u live? ")
+country = input("enter your country's name? ")
+hobby = input("enter your hobbies ")
+favorite_hobby = input("enter the one u love doing the most? ")
+time  = input("how long have u been doing it? ")
+experiences = input("what are your experiences? ")
+challengews = input("what are the challenges you faced in your early stages of doing it? ")
+merits = input("what are the advantages you have received using this ur hobby as your weapon? ")
+what_u_love_about_it = input("what do u love the most about it? ")
+future = input("how long do u plan on doing it? ")
+advice = input("any advice u might want to give the people following ur footsteps? ")
+
+
+print()
+print("\n--------------------------------------------My Hobby Story-------------------------------------------------")
+print(f"Hello everyone, my name is {full_name}. i am {age} years old.  I live {residence}. I am a citizen of {country}.\n    My hobbies are  {hobby}, but the one i love the most is {favorite_hobby}. I have been doing this for {time} now. During my time in this i have experienced a lot of things like; {experiences}. Also during all this while i have been doing my favorite thing sincerely speaking,i had alot of challenges at the first stages. challewnges like; {challengews}  while also having experience this challenges as time went on i started gaining merits like; {merits}. In this my {favorite_hobby}, what i love the most about it is that, {what_u_love_about_it} and doing it gives me alot of joy and strength to pass through any phase i encounter all the past, present, and future times i will still be doing it cause it has become a part of me.  I plan on doing this for as long as {future}. Well my candid advice to all those journeying with me in this my favorite hobby is {advice}. Even if my favorite isn't your favorite i hope that you are always happy in what ever thing u love doing. and remember hobbies are meant to always make u happy 😍👌😆🤗.")
+print("----------------------------Do Have A Happy Hobby Story Like Mine❤️ ❤️ ❤️ ❤️---------------------------------------" )
+
+
+
+
+
+       
+      
